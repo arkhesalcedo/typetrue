2815bc8aa40f845685c3a938c56acd48604024ce
Arkhe Salcedo <arkhe@isi.ca>
2021-03-29 11:51:10 -0400

    Add heap code

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -41,6 +41,12 @@
         })(window,document,'script','dataLayer','GTM-MZBHRFP');</script>
     <!-- End Google Tag Manager -->
 
+    <!-- Heap -->
+    <script type="text/javascript">
+      window.heap=window.heap||[],heap.load=function(e,t){window.heap.appid=e,window.heap.config=t=t||{};var r=document.createElement("script");r.type="text/javascript",r.async=!0,r.src="https://cdn.heapanalytics.com/js/heap-"+e+".js";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(r,a);for(var n=function(e){return function(){heap.push([e].concat(Array.prototype.slice.call(arguments,0)))}},p=["addEventProperties","addUserProperties","clearEventProperties","identify","resetIdentity","removeEventProperty","setEventProperties","track","unsetEventProperty"],o=0;o<p.length;o++)heap[p[o]]=n(p[o])};
+      heap.load("2626990738");
+    </script>
+
     <!-- Facebook Pixel Code -->
     <script>
         !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
