323f0e25a51e09567205726e9f8a862470a7968d
Arkhe Salcedo <arkhe@isi.ca>
2021-05-12 12:26:21 -0400

    Update google tags

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -35,10 +35,10 @@
 
     <!-- Google Tag Manager -->
     <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-MZBHRFP');</script>
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-529ZJD6');</script>
     <!-- End Google Tag Manager -->
 
     <!-- Heap -->
@@ -132,7 +132,7 @@
 
 <body>
     <!-- Google Tag Manager (noscript) -->
-    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MZBHRFP"
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-529ZJD6";
                       height="0" width="0" class="tw-hidden tw-invisible"></iframe></noscript>
     <!-- End Google Tag Manager (noscript) -->
     <div id="app" class="tw-min-h-screen tw-flex tw-flex-col tw-justify-between @yield('pageClass')">
